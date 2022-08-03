b77a04bdf74987b8a9f482ab94950cf302aff5a9
AviCalidas-design <acalidas@uci.edu>
2022-08-03 09:31:11 -0700

    Even/Odd and Prime

---
+++ number-checker/number-checker/src/main/java/com/revature/App.java
@@ -0,0 +1,39 @@
+package com.revature;
+import java.util.Scanner;
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        Scanner scan = new Scanner(System.in);
+        String input = scan.nextLine();
+        int val = Integer.parseInt(input);
+        if(val % 2 == 0)
+        {
+            System.out.println( "Even" );
+        }
+        else
+        {
+            System.out.println( "Odd" );
+        }
+        boolean prime = true;
+        for(int i = 2; i <= val / 2.0 ; i++)
+        {
+            if(val % i == 0)
+            {
+                prime = false;
+            }
+        }
+        if(prime)
+        {
+            System.out.println( "Prime" );
+        }
+        else
+        {
+            System.out.println( "Not prime" );
+        }
+    }
+}
