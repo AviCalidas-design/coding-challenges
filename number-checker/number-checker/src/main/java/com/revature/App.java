23e39104b5cabf3e9661710b6641f7b1c128e969
AviCalidas-design <acalidas@uci.edu>
2022-08-05 08:26:31 -0700

    Pig Latin

--- number-checker/number-checker/src/main/java/com/revature/App.java
+++ number-checker/number-checker/src/main/java/com/revature/App.java
@@ -10,30 +10,33 @@ public class App
     {
         Scanner scan = new Scanner(System.in);
         String input = scan.nextLine();
-        int val = (int)Integer.parseInt(input);
-        if(val % 2 == 0)
-        {
-            System.out.println( "Even" );
-        }
-        else
-        {
-            System.out.println( "Odd" );
-        }
-        boolean prime = true;
-        for(int i = 2; i <= val / 2.0 ; i++)
-        {
-            if(val % i == 0)
-            {
-                prime = false;
-            }
-        }
-        if(prime)
-        {
-            System.out.println( "Prime" );
-        }
-        else
-        {
-            System.out.println( "Not prime" );
-        }
+        char first = input.charAt(0);
+        String cut = input.substring(1);
+        System.out.println((cut + first) + "ay");
+        // int val = (int)Integer.parseInt(input);
+        // if(val % 2 == 0)
+        // {
+        //     System.out.println( "Even" );
+        // }
+        // else
+        // {
+        //     System.out.println( "Odd" );
+        // }
+        // boolean prime = true;
+        // for(int i = 2; i <= val / 2.0 ; i++)
+        // {
+        //     if(val % i == 0)
+        //     {
+        //         prime = false;
+        //     }
+        // }
+        // if(prime)
+        // {
+        //     System.out.println( "Prime" );
+        // }
+        // else
+        // {
+        //     System.out.println( "Not prime" );
+        // }
     }
 }
